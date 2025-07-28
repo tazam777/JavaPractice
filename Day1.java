1eba205d395da1827f4c7625c6b54ea212a08881
Tarique Anwar Mulla <tariquemulla14@gmail.com>
2025-07-28 16:35:45 -0600

    Day 1 Solved

---
+++ Day1.java
@@ -0,0 +1,28 @@
+/*Key Coding Question (Day 1)
+
+Can you declare and initialize basic variables in Java and print them in a formatted message?"
+
+So variable is basically telling java to label some space in the memory so that we can store some stuff in it 
+declating means what type of data we storing data can be int,string,bool.long,double
+we say that to java because java is statically typed and it need to be told before hand what we plan to doing to spcae in momory
+initilizaing is storing some value into that space 
+
+*/
+
+
+
+
+
+
+
+
+public class Day1 {
+    public static void main(String[] args) {
+        
+
+        int age=28;
+        String name="Tarique";
+        System.out.println("Hi my name is " + name + "\n" + "I am " + age + " years old. ");
+    }
+    
+}
